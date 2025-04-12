513a9f419da2da8273285e477902e04950aa4ab9
Akshit-bhardwaj <akshitbhardwaj2424@gmail.com>
2025-04-12 14:14:36 +0530

    Co-authored-by: Agrit Tiwari <agrittiwari@users.noreply.github.com>

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,14 +29,21 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'Counter-App',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
+          title: 'Stop-Watch',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+         <Tabs.Screen
+        name="timerApp"
+        options={{
+          title: 'Timer',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,93 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+// Make an counter app
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+// Make a component called Counter
+function Counter(){
+  // We use useState(0) to initialise the value of count=0.
+  const[count , setCount]=useState(0);
+  
+  // We need to return the component to show on the browser
+return(
+  // Make a   called view , It is same as <div></div>
+  <View>
+    {/* We create a <Text></Text> which is as similar as the tags of html like <p> <h1>. */}
+    <Text style={styles.counterText}>Count : {count} </Text>
+    {/* To display the value of state , We need to make a button */}
+    <View style={styles.buttonContainer}>
+    <View style={styles.button1}>
+    <Button  title="+" onPress={()=>setCount(count+1)}/>
+    </View>
+    <View style={styles.button2}>
+    <Button title="-" onPress={()=>setCount(count-1)}/>
+    </View>
+  </View>
+  <View style={styles.button3}>
+      <Button title="RESET" onPress={() => setCount(0) }/>
+    </View>
+  </View>
+
+);
 }
-
+// We use this as a design of our Home Screen.
+function HomeScreen(){
+  return(
+    <View style = {styles.container}>
+      <Counter/>
+      </View>
+  )
+}
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
+    backgroundColor: 'black',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  counterText: {
+    fontSize: 50,
+    marginBottom: 20,
+    fontWeight:'bold',
+    alignItems:'center',
+    color:'white',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: 100,
+    height:50,
+    marginTop: 20,
+    marginBottom: 10,
+    gap: 20,
+    marginLeft:50,
+    alignItems:'center',
   },
+  button1: {
+    borderRadius: 100,
+    width: 50,
+    marginTop:10,
+  },
+  button2: {
+    borderRadius: 10,
+    width: 50,
+    marginTop:10,
+  },
+  button3: {
+    // backgroundColor: 'black',
+    borderRadius: 200,
+    marginBottom: 80,
+    alignSelf:'center',
+    width:100,
+    
+  }
+  // buttonText: {
+  //   color: 'white',
+  //   fontSize: 18,
+  // }
+  // // resetText: {
+  // //   color: 'white',
+  // //   fontSize: 16,
+  // // }
 });
+export default HomeScreen;
